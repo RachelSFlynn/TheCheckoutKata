51c94dd76b6f063bfdf54d621f92194a0396cd5f
GingerPhysicist6 <55626027+GingerPhysicist6@users.noreply.github.com>
2024-09-03 19:34:23 +0100

    feat: moving Product and Pricing to new namespace

--- Domain/Products/PricingRules.cs
+++ Domain/Products/PricingRules.cs
@@ -1,4 +1,4 @@
-﻿namespace Domain;
+﻿namespace Domain.Products;
 
 public class PricingRules
 {
@@ -10,14 +10,14 @@ public class PricingRules
     /// <summary>
     /// Gets or sets the discount price.
     /// </summary>
-    public int SpecialPrice { get; set; }
+    public decimal SpecialPrice { get; set; }
 
     /// <summary>
     /// Gets or sets a list of products linked in the offer.
     /// </summary>
     public IList<Product>? LinkedProducts { get; set; }
 
-    public PricingRules(int specialQuantity, int specialPrice)
+    public PricingRules(int specialQuantity, decimal specialPrice)
     {
         SpecialQuantity = specialQuantity;
         SpecialPrice = specialPrice;
---
+++ Domain/Products/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain.Products;
+
+public class Product
+{
+    /// <summary>
+    /// Gets or sets the unit price.
+    /// </summary>
+    public decimal UnitPrice { get; set; }
+
+    /// <summary>
+    /// Gets or sets the pricing rules for the item.
+    /// </summary>
+    public IList<PricingRules>? Offers { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the item.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the item.
+    /// </summary>
+    public string? Description { get; set; }
+
+    public Product(decimal unitPrice, string name, string description = "No description given")
+    {
+        Name = name;
+        UnitPrice = unitPrice;
+        Description = description;
+    }
+}
