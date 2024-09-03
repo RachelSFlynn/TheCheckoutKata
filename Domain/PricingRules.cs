2e96c96a9bab3fa6bd6e7748574c4df53b2d6735
GingerPhysicist6 <55626027+GingerPhysicist6@users.noreply.github.com>
2024-09-03 18:53:38 +0100

    feat: adding Products with pricing rules
    Added Product for a shop which have a SKU, name, description, unit price and PricingRules. Pricing rules are any offers that are on the product.

---
+++ Domain/PricingRules.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain;
+
+public class PricingRules
+{
+    /// <summary>
+    /// Gets or sets the quantity needed for discount.
+    /// </summary>
+    public int SpecialQuantity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the discount price.
+    /// </summary>
+    public int SpecialPrice { get; set; }
+
+    /// <summary>
+    /// Gets or sets a list of products linked in the offer.
+    /// </summary>
+    public IList<Product>? LinkedProducts { get; set; }
+
+    public PricingRules(int specialQuantity, int specialPrice)
+    {
+        SpecialQuantity = specialQuantity;
+        SpecialPrice = specialPrice;
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain;
+
+public class Product
+{
+    /// <summary>
+    /// Gets or sets the Primary Key for the item.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Stock Keeping Units for the item.
+    /// </summary>
+    public string SKU { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the item.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the unit price.
+    /// </summary>
+    public int UnitPrice { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the item.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the pricing rules for the item.
+    /// </summary>
+    public IList<PricingRules> Offers { get; set; }
+
+    public Product(string sku, string name, int unitPrice, string description = "No description given")
+    {
+        SKU = sku;
+        Name = name;
+        UnitPrice = unitPrice;
+        Description = description;
+    }
+}
