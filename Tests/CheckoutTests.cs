6dc1587dfbad41a5a9612f881d16283d158f5d18
GingerPhysicist6 <55626027+GingerPhysicist6@users.noreply.github.com>
2024-09-04 07:37:51 +0100

    feat: finalising checkout unit tests

--- Tests/CheckoutTests.cs
+++ Tests/CheckoutTests.cs
@@ -14,9 +14,7 @@ public class CheckoutTests
         var pricingRulesA = new List<PricingRule>
         {
             { new PricingRule(3, 50) },
-            { new PricingRule(4, 40) },
-            { new PricingRule(5, 30) },
-            { new PricingRule(6, 20) }
+            { new PricingRule(4, 40) }
         };
 
         var pricingRulesB = new List<PricingRule>
@@ -37,7 +35,23 @@ public class CheckoutTests
     }
 
     [Fact]
-    public void Checkout_ProductWithNoOffers_CorrectPrice()
+    public void ScanWithSubTotal_ProductWithNoOffers_CorrectPrice()
+    {
+        // Arrange
+        decimal expectedPrice = 70 * 3;
+
+        // Act
+        _checkout.Scan("B");
+        _checkout.Scan("B");
+        _checkout.Scan("B");
+        decimal totalPrice = _checkout.GetSubTotal();
+
+        // Assert
+        totalPrice.Should().Be(expectedPrice);
+    }
+
+    [Fact]
+    public void ScanWithTotal_ProductWithNoOffers_CorrectPrice()
     {
         // Arrange
         decimal expectedPrice = 80 * 3;
@@ -52,20 +66,57 @@ public class CheckoutTests
         totalPrice.Should().Be(expectedPrice);
     }
 
-
-    [Fact]
-    public void Checkout_ProductWithOffers_CorrectPrice()
+    [Theory]
+    [InlineData(50, "B", "B", "B")]
+    [InlineData(190, "C", "B", "A", "B", "B")]
+    [InlineData(40, "A", "A", "A", "A")]
+    [InlineData(100, "A", "A", "A", "A", "A")]
+    [InlineData(90, "A", "B", "A", "B", "A", "B", "A")]
+    public void ScanWithTotal_ProductWithOffers_CorrectPrice(decimal expectedPrice, params string[] products)
     {
         // Arrange
-        decimal expectedPrice = 50;
 
         // Act
-        _checkout.Scan("B");
-        _checkout.Scan("B");
-        _checkout.Scan("B");
+        foreach (var product in products)
+        {
+            _checkout.Scan(product);
+        }
         decimal totalPrice = _checkout.GetTotalPrice();
 
         // Assert
         totalPrice.Should().Be(expectedPrice);
+    }
+
+    [Fact]
+    private void ScanProduct_ProductNotAdded_ThrowsException()
+    {
+        // Arrange
+        var productSku = "Invalid";
+
+        // Act
+        Action act = () => _checkout.Scan(productSku);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage($"Failed to scan {productSku} with error: Invalid SKU: {productSku}");
+    }
+
+    [Fact]
+    public void ScanProduct_ShouldAddProductToScannedProduct_WhenProductIsValid()
+    {
+        // Arrange
+        var productSku = "Scan";
+        var products = new Dictionary<string, Product>
+        {
+            { productSku, new Product(10.0m, new List<PricingRule>(), "Scan should add product") }
+        };
+        var checkout = new Checkout(products);
+
+        // Act
+        checkout.Scan(productSku);
+
+        // Assert
+        checkout.ScannedItems.Should().ContainKey(productSku);
+        checkout.ScannedItems[productSku].Should().Be(1);
     }
 }
